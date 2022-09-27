98ff08eaee98faecd832df186af42f512fe39b78
marcelo <marcelocamusso@hotmail.com>
2022-09-27 17:39:43 -0300

    feat: navbar tema claro

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Navbar from './componentes/Navbar'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>    
    
-   <App />
+   <Navbar />
+   
 
       
   </React.StrictMode>
