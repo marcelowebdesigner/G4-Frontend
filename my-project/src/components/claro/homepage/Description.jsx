450ebd8e9d2a58b15c9ef6a9919d475b489995ee
marcelo <marcelocamusso@hotmail.com>
2022-10-04 17:27:53 -0300

    feat:description:homepage_light

---
+++ my-project/src/components/claro/homepage/Description.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+/** Esta es la descripcion del homepage en modo claro. 
+ * 
+ */
+
+function Description() {
+  return ( 
+      
+    <div class="flex justify-center">
+      <h1 className="text-black-800 text-3xl">Don't forget to cancel your subscriptions in time</h1>  
+    </div>
+   
+    
+  )
+}
+export default Description
