d08acde534b97b8b53bb107e122d0c302dfb2c85
marcelo <marcelocamusso@hotmail.com>
2022-10-06 17:49:57 -0300

    docs:implementar router router dom 6

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Description from "./components/homepage/Description";
 import SectionCards from "./components/homepage/SectionCards";
 import Header from "./components/homepage/Header";
@@ -7,12 +8,13 @@ import "./index.css";
 
 const HomePage = () => {
   return (
-    <>
+    <React-Fragment>
       <Navbar />
+      
       <Header />
       <Description />
       <SectionCards/>
-    </>
+    </React-Fragment>
   );
 };
 
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './App'
-
-
+import {BrowserRouter as Route} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>    
-    <HomePage/>
+  
+      
+        <Route exact path="/home">
+          <HomePage />
+        </Route>   
+
+
   </React.StrictMode>
   
 )
