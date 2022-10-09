034f2048791c3fb70b839940531ee9f0f43c936e
proyect_icaro <josuevalenzuela87@gmail.com>
2022-10-09 01:07:30 -0300

    bugfix:Modificaciones en main.js

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {HomePage, LoginPage} from './App'
-import {BrowserRouter as Route} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
+import AppRouting from './AppRouting'
 
 
 
@@ -10,10 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>    
   
       
-        <Route exact path="/home">
-          <HomePage />
-        </Route> 
-        <Route exact path="/login"><LoginPage/></Route>
+        <BrowserRouter>
+          <AppRouting/>
+        </BrowserRouter>
         
 
 
