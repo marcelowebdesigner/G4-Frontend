1da88b3393dc5ebb14ef1bff8007a5f3e4187f25
Diego <108905445+diego-vecch@users.noreply.github.com>
2022-10-08 21:46:39 -0300

    Revert "bigfix:AppRouting y ContactForm ademas de nueva carpeta pages a la al…"

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import AppRouting from './AppRouting'
-
+import {HomePage, LoginPage} from './App'
+import {BrowserRouter as Route} from 'react-router-dom'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>    
-  <BrowserRouter>
-     <AppRouting/>
-  </BrowserRouter> 
+  
+      
+        <Route exact path="/home">
+          <HomePage />
+        </Route> 
+        <Route exact path="/login"><LoginPage/></Route>
         
+
+
   </React.StrictMode>
   
 )
--- my-project/src/pages/HowItWork.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HowItWork = () => {
-  return (
-    <div>HowItWork</div>
-  )
-}
-
-export default HowItWork
