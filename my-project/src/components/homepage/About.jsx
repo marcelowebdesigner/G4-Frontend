f163557b09011486b133915de71773826b239f15
marcelo <marcelocamusso@hotmail.com>
2022-10-12 11:41:01 -0300

    feat:aboutUs, create view;

---
+++ my-project/src/components/homepage/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/** Este es el contenido de about en modo claro. 
+ * 
+ */
+
+function ContenidoAbout() {
+  return ( 
+      
+    <div class="columns-2 pb-6">
+        <img class="w-full aspect-video" src="./src/assets/media/svg/img-about.svg"/>
+    
+        <h1 className="bg-purple-400 text-black-800 font-sans py-8 px-8">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+        </h1>  
+
+    </div>
+   
+    
+  )
+}
+export default ContenidoAbout
