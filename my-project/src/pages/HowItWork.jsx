c0b338a5736ca151acbd33e731c8c93399b563c8
marcelo <marcelocamusso@hotmail.com>
2022-10-18 14:17:14 -0300

    bugfix:howitwork bg-light

--- my-project/src/pages/HowItWork.jsx
+++ my-project/src/pages/HowItWork.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Footer from '../components/homepage/Footer';
 import HowItWorkContent from '../components/homepage/HowItWorkContent';
 import Navbar from '../components/homepage/Navbar';
+import '../styles/Image-bg-ligth.css'
 
 const HowItWork = () => {
   return (
-    <div>
+    <div className="bg-light">
       <Navbar />
       <HowItWorkContent />
       <Footer />
